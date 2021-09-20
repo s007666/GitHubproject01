d1036f8ce515c81f481ef97470cff6eb3adec4c4
s007744 <s007744@dtu.dk>
2021-09-20 15:32:36 +0200

    Changed Tekst

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// Test af fælles GitHub project
+        // Test af fælles GitHub project
+        System.out.println("Hej Gruppe 1 kan i se dette!?");
     }
 }
